413db34075682b2c98ec0e7f1e334b4fe773626f
Gabriella <84185597+GabriellaLella@users.noreply.github.com>
2022-08-29 23:16:57 +0200

    Create gitdobot.js

---
+++ dono/gitdobot.js
@@ -0,0 +1,57 @@
+const gitdobot = (prefisso) => {
+ritorno`
+​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​
+
+Ricorda solo che devi dare i comandi di base di termux prima, per inizializzare il bot, digitare:
+
+${prefix}comandi-termux
+
+───────────────
+
+[Come installare Bot Aleatory Multi-Device in termux:
+
+Apri Termux => {
+  
+  
+- 1 comando >
+
+
+cd /scheda SD/
+
+
+- 2 Comando >
+
+
+git clone https://github.com/NuevaGeneracionALB/aleatory-md
+
+
+- 3 Comando >
+
+
+cd-md casuale
+
+
+- 4 Comando >
+
+
+sh start.sh
+
+
+Quest'ultimo comando, inizializzerà il bot, e genererà il qrcode, ti servirà un secondo cellulare, per fare una foto del qrcode e scansionare la foto che hai fatto, con un secondo whatsapp, può essere della categoria bussines, vai ai dispositivi di connessione e, se hai una scelta lì, scegli di entrare nella versione beta, quindi Scansiona, la foto scattata che è sull'altro telefono, buona fortuna.
+  
+  
+  Se vuoi configurare il Bot, il numero del Proprietario, ecc.
+  
+  Digita il seguente comando:
+  
+  
+  ${prefix}config-bot
+  
+  
+}]
+
+───────────────
+`
+}
+
+exports.gitdobot = gitdobot
